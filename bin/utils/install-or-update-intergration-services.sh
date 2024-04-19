c13ffab3fcc84a43bd3baec906110d2cf8c5ba5d
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-04-19 09:12:44 +0100

    Fixed integration service script to pull in latest changes on update

--- bin/utils/install-or-update-intergration-services.sh
+++ bin/utils/install-or-update-intergration-services.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/../.." || exit
 
 if [ -d $integration_services_dir ]; then
   echo "==> Updating hmpps-probation-integration-services..."
-  cd $integration_services_dir && git fetch > /dev/null 2>&1
+  cd $integration_services_dir && git pull > /dev/null 2>&1
 else
   echo "==> Cloning hmpps-probation-integration-services..."
   git clone git@github.com:ministryofjustice/hmpps-probation-integration-services.git
