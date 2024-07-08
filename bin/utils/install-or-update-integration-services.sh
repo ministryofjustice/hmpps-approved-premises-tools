ee02dfe64b0392ea958237f925a279fd0bfaedf7
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-07-08 17:18:35 +0100

    Update probation-integration-service on —refresh

--- bin/utils/install-or-update-integration-services.sh
+++ bin/utils/install-or-update-integration-services.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+echo "==> Clone or update hmpps-probation-integration-services ..."
+
 integration_services_dir="hmpps-probation-integration-services"
 
 cd "$(dirname "$0")/../.." || exit
 
 if [ -d $integration_services_dir ]; then
   echo "==> Updating hmpps-probation-integration-services..."
-  cd $integration_services_dir && git pull > /dev/null 2>&1
+  cd $integration_services_dir && git pull
 else
   echo "==> Cloning hmpps-probation-integration-services..."
   git clone https://github.com/ministryofjustice/hmpps-probation-integration-services.git
--- bin/utils/refresh-containers.sh
+++ bin/utils/refresh-containers.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+"$(dirname "$0")/install-or-update-integration-services.sh"
+
 echo "==> Refreshing containers..."
 
 yq '.services[].image' "$(dirname "$0")/../../docker-compose.yml" | while read -r container; do docker pull "$container"; done
