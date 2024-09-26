cf7c8b3243a60b2c997a80e71f1676bed7868211
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-09-26 09:12:04 +0100

    Rename ‘nomis-db’ folder to ‘prison-api’
    nomis-db is technically correct, but prison-api more user friendly when trying to figure out which database to clear for prison-api

--- bin/utils/refresh-containers.sh
+++ bin/utils/refresh-containers.sh
@@ -6,6 +6,3 @@ echo "==> Refreshing containers..."
 
 yq '.services[].image' "$(dirname "$0")/../../docker-compose.yml" | while read -r container; do docker pull "$container"; done
 
-rootpath="$(dirname "$0")/../../"
-rm -rf "$rootpath/community-api-db" && \
-rm -rf "$rootpath/nomis-db"
