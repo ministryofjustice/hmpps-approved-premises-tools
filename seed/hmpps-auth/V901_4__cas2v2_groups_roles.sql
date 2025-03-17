98f6da10a534f6e85b884dbce3a3672f70dd87e5
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-03-17 16:17:56 +0000

    fix: updated toles to have _REFERRER on the end

--- seed/hmpps-auth/V901_4__cas2v2_groups_roles.sql
+++ seed/hmpps-auth/V901_4__cas2v2_groups_roles.sql
@@ -1,22 +1,22 @@
--- create CAS2_COURT_BAIL group for delius users
+-- create CAS2_COURT_BAIL_REFERRER group for delius users
 INSERT INTO groups (group_id, group_code, group_name, create_datetime)
-VALUES ('a274e977-4ff8-43cf-9334-5b4703c1aff7', 'CAS2_COURT_BAIL', 'CAS2 COURT BAIL', '2023-10-15 21:35:52.043333');
+VALUES ('a274e977-4ff8-43cf-9334-5b4703c1aff7', 'CAS2_COURT_BAIL_REFERRER', 'CAS2 COURT BAIL REFERRER', '2023-10-15 21:35:52.043333');
 
 -- create CAS2 Court Bail role
---   This will appear as the ROLE_CAS2_COURT_BAIL authority in the JWT
+--   This will appear as the ROLE_CAS2_COURT_BAIL_REFERRER authority in the JWT
 --   and can be referenced with either hasRole("CAS2_COURT_BAIL") or
 --   hasAuthority("ROLE_CAS2_COURT_BAIL") in Spring Security filters
 INSERT INTO roles (role_id, role_code, role_name, create_datetime, role_description, admin_type)
-VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb18', 'CAS2_COURT_BAIL', 'CAS2 COURT BAIL', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
+VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb18', 'CAS2_COURT_BAIL_REFERRER_REFERRER', 'CAS2 COURT BAIL REFERRER', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
 
--- create CAS2_PRISON_BAIL group for delius users
+-- create CAS2_PRISON_BAIL_REFERRER group for delius users
 INSERT INTO groups (group_id, group_code, group_name, create_datetime)
-VALUES ('a274e977-4ff8-43cf-9334-5b4703c1afa7', 'CAS2_PRISON_BAIL', 'CAS2 PRISON_BAIL', '2023-10-15 21:35:52.043333');
+VALUES ('a274e977-4ff8-43cf-9334-5b4703c1afa7', 'CAS2_PRISON_BAIL_REFERRER', 'CAS2 PRISON_BAIL REFERRER', '2023-10-15 21:35:52.043333');
 
 -- create CAS2 Court Bail role
 --   This will appear as the ROLE_CAS2_COURT_BAIL authority in the JWT
---   and can be referenced with either hasRole("CAS2_COURT_BAIL") or
---   hasAuthority("ROLE_CAS2_COURT_BAIL") in Spring Security filters
+--   and can be referenced with either hasRole("CAS2_COURT_BAIL_REFERRER") or
+--   hasAuthority("ROLE_CAS2_COURT_BAIL_REFERRER") in Spring Security filters
 INSERT INTO roles (role_id, role_code, role_name, create_datetime, role_description, admin_type)
-VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb68', 'CAS2_PRISON_BAIL', 'CAS2 PRISON BAIL', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
+VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb68', 'CAS2_PRISON_BAIL_REFERRER', 'CAS2 PRISON BAIL REFERRER', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
 
