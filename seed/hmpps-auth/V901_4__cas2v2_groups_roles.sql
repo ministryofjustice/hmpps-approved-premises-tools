b93fe5ce3eff1fc15df49614dea5811894441239
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-03-18 11:16:48 +0000

    fix: Delius prison bail referrer

--- seed/hmpps-auth/V901_4__cas2v2_groups_roles.sql
+++ seed/hmpps-auth/V901_4__cas2v2_groups_roles.sql
@@ -7,7 +7,7 @@ VALUES ('a274e977-4ff8-43cf-9334-5b4703c1aff7', 'CAS2_COURT_BAIL_REFERRER', 'CAS
 --   and can be referenced with either hasRole("CAS2_COURT_BAIL") or
 --   hasAuthority("ROLE_CAS2_COURT_BAIL") in Spring Security filters
 INSERT INTO roles (role_id, role_code, role_name, create_datetime, role_description, admin_type)
-VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb18', 'CAS2_COURT_BAIL_REFERRER_REFERRER', 'CAS2 COURT BAIL REFERRER', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
+VALUES ('f62b8a99-00c7-4250-8279-3eb80129eb18', 'CAS2_COURT_BAIL_REFERRER', 'CAS2 COURT BAIL REFERRER', '2023-10-15 21:35:51.130000', null, 'EXT_ADM');
 
 -- create CAS2_PRISON_BAIL_REFERRER group for delius users
 INSERT INTO groups (group_id, group_code, group_name, create_datetime)
