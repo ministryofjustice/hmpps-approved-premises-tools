6c701521a1d560b73d93fe42cee295f067fd90cd
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2025-04-02 12:34:15 +0100

    Simplify ap-tools

--- bin/utils/refresh-containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-"$(dirname "$0")/install-or-update-integration-services.sh"
-
-echo "==> Refreshing containers..."
-
-yq '.services[].image' "$(dirname "$0")/../../docker-compose.yml" | while read -r container; do docker pull "$container"; done
-
