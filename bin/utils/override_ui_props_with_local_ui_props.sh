29b217832ae045ed6583c584b559c037bb724b42
Greg Hawkins <greg.hawkins@digital.justice.gov.uk>
2025-04-10 16:38:50 +0100

    Override .env-ui props with LOCAL_CAS_UI_PATH repo's .env props

---
+++ bin/utils/override_ui_props_with_local_ui_props.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+# shellcheck disable=SC3040
+set -o pipefail
+
+# Override the properties we have in target with anything included in the .env file in the local UI repo
+override_ui_props_with_local_ui_props() {
+  echo "==> Override the properties we have in .env.ui file with anything included in the .env file in the local UI repo"
+
+  target_env=$1 ; shift
+  priority_env="${LOCAL_CAS_UI_PATH}/.env"
+  merged_env=".env.merged"
+
+  echo "Overriding $target_env properties with $priority_env properties"
+
+  # Extract keys from priority file
+  priority_keys=$(cut -d '=' -f 1 "$priority_env")
+
+  # Remove lines from .env that are overridden
+  cp "$target_env" "$merged_env"
+  for key in $priority_keys; do
+      # Remove lines with matching key (supports optional whitespace)
+      sed -i '' "/^$key[[:space:]]*=/d" "$merged_env"
+  done
+
+  # Append all priority variables to output
+  cat "$priority_env" >> "$merged_env"
+
+
+  # Replace the content $$target_env file with the content of the merged .env file
+  cp "$merged_env" "$target_env"
+
+  # Clean up
+  rm -f "$merged_env.bak"
+  rm "$merged_env"
+
+  echo "Merged .env file created at $target_env"
+}
