1c888bac477186a5b733801aa2d490c04cbfa734
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-04-14 16:38:17 +0100

    Remove need for checked out bootstraped ui code
    Before this change when running a UI via docker the corresponding code must still be checked out and the `.env` file bootstrapped.
    
    This commit fixes that issue by introducing a separate `.env.ui` file for each user interface, and adding a mandatory command line argument to indicate which UI is being used e.g.
    
    ```ap-tools server start --cas1```
    
    This also fixed a bug that meant we weren’t actually stopping locally running UIs when running `ap-tools stop`. This is because we force killed the tilt process (`using killall -9`)

--- bin/utils/override_ui_props_with_local_ui_props.sh
+++ bin/utils/override_ui_props_with_local_ui_props.sh
@@ -3,6 +3,11 @@ set -e
 # shellcheck disable=SC3040
 set -o pipefail
 
+#
+# Note! this script is not currently used. If it is, configuration needs to come from a location
+# other than LOCAL_CAS_UI_PATH, which is no longer required
+#
+
 # Override the properties we have in target with anything included in the .env file in the local UI repo
 override_ui_props_with_local_ui_props() {
   echo "==> Override the properties we have in .env.ui file with anything included in the .env file in the local UI repo"
@@ -32,7 +37,6 @@ override_ui_props_with_local_ui_props() {
   cp "$merged_env" "$target_env"
 
   # Clean up
-  rm -f "$merged_env.bak"
   rm "$merged_env"
 
   echo "Merged .env file created at $target_env"
